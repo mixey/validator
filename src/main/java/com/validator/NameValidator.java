4f121733a98d381d0c3943603b1910e42196dbc8
Mikhail T <m.tkachev@magdv.com>
2017-06-15 09:50:29 +0700

    * Improved NameValidator

--- src/main/java/com/validator/NameValidator.java
+++ src/main/java/com/validator/NameValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NameValidator extends BaseValidator<String> {
-    private final String PATTERN = "^[a-zA-ZА-Яа-я]{3,20}(-[a-zA-ZА-Яа-я]{3,20})?$";
+    private final String PATTERN = "^[a-zA-ZА-Яа-яёЁ]{3,20}(-[a-zA-ZА-Яа-яёЁ]{3,20})?$";
     private final String DEFAULT_ERROR_MESSAGE = "Неверный формат";
 
     public NameValidator(int id) {
